4905488c3ffbffc635ae8951f14a1c3703e031c2
arifrahmanto <arifismiadi.rahmanto@mitrais.com>
2019-01-23 14:48:41 +0700

    default Argument

---
+++ defaultArg.js
@@ -0,0 +1,12 @@
+function sum(a=0, b=0) {
+  return a + b;
+}
+
+console.log(`Sum with empty param:  ${sum()}`);
+console.log(`Sum with param:  ${sum(2,4)}`);
+
+function addOffset(style = {}) {
+  style.offset = '10px';
+  return style;
+}
+
