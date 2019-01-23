9d06af6603f518902af83fb57dd1331985b33a81
arifrahmanto <arifismiadi.rahmanto@mitrais.com>
2019-01-23 10:44:10 +0700

    arrow function and template string

---
+++ arrowfunc.js
@@ -0,0 +1,13 @@
+const fibonacci = n => {
+  if (n < 3) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(`Fibonacci 10: ${fibonacci(10)}`);
+
+const profile = {
+  name: 'Alex',
+  getName: () => {
+      return profile.name 
+  }
+};
+console.log(`Profile : ${profile.getName()}`);
