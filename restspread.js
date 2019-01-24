d48851fc84f764c95e7e4ec12bbd71a4d0db7e2a
arifrahmanto <arifismiadi.rahmanto@mitrais.com>
2019-01-24 09:40:26 +0700

    update restspread example

--- restspread.js
+++ restspread.js
@@ -8,7 +8,9 @@ console.log(product(1, 2, 3, 4, 5));
 function join(array1, array2) {
   return [...array1, ...array2];
 }
+console.log(join([1,2,3],[3,4,5]));
 
-function unshift(array, ...numbers) {
-  return [...numbers, ...array];
+function shift(array, ...numbers) {
+  return [...array, ...numbers,];
 }
+console.log(shift([1,2,3],3,4,5));
